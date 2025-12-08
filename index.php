f2e8c94c81a6bc103f8da7b94260348466ce66d0
dindapus <sitizulaikha221133@gmail.com>
2025-12-08 23:03:36 +0700

    [add] menambah halaman about

--- index.php
+++ index.php
@@ -148,6 +148,27 @@ $_SESSION["history"][] = $fact_id;
     </div>
 </div>
 
+<!-- ==================== ABOUT SECTION ======================= -->
+<div id="about" class="page-section container mt-5">
+    <div class="card shadow-lg rounded-4">
+        <div class="card-body">
+            <h3>ℹ️ About</h3>
+            <p>Aplikasi ini menampilkan fakta kucing menggunakan REST Client dari API catfact.ninja.</p>
+        </div>
+    </div>
+</div>
+
+<!-- ==================== ABOUT SECTION ======================= -->
+<div id="about" class="page-section container mt-5">
+    <div class="card shadow-lg rounded-4">
+        <div class="card-body">
+            <h3>ℹ️ About</h3>
+            <p>Aplikasi ini menampilkan fakta kucing menggunakan REST Client dari API catfact.ninja.</p>
+        </div>
+    </div>
+</div>
+
+
 <!-- ==================== JS ======================= -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
